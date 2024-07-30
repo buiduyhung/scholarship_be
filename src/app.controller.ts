129a40fba9fa4494af4fe70945c47cd5add640c0
Le Cong Anh <huuanh952002@gmail.com>
2024-07-31 00:24:40 +0700

    JWT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { Public } from './decorator/customize';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private ConfigService: ConfigService
+    private ConfigService: ConfigService,
+    private authService: AuthService
   ) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  handleLogin(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
