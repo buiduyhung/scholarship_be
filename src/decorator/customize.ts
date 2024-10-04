abaf3e1914e7be44558972a84f64151a04b713fa
vansang <sangnvde160492@fpt.edu.vn>
2024-10-04 16:21:49 +0700

    test1

--- src/decorator/customize.ts
+++ src/decorator/customize.ts
@@ -14,6 +14,6 @@ export const User = createParamDecorator(
         return request.user;
     },
 );
-
+//sang da comment
 export const IS_PUBLIC_PERMISSION = 'isPublicPermission';
 export const SkipCheckPermission = () => SetMetadata(IS_PUBLIC_PERMISSION, true);
