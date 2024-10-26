963f5825b94fa8471574e385b50e0f53e7af71b4
thinhlaemdo <thinhnqde160023@fpt.edu.vn>
2024-10-26 21:35:20 +0700

    26/10 thinhnq

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,3 +1,4 @@
+// Import các module và dịch vụ cần thiết
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ChangePasswordDto, CreateUserDto } from './dto/create-user.dto';
@@ -6,13 +7,13 @@ import { IUser } from './users.interface';
 import { Public, ResponseMessage, User } from 'src/decorator/customize';
 import { ApiTags } from '@nestjs/swagger';
 
-
-
+// Đánh dấu các endpoint của controller liên quan đến user
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
+  // Endpoint để tạo người dùng mới
   @Post()
   @ResponseMessage("Create a new user")
   async create(
@@ -24,6 +25,7 @@ export class UsersController {
     };
   }
 
+  // Endpoint để lấy danh sách người dùng
   @Get()
   @ResponseMessage("Fetch List Users with paginate")
   findAll(
@@ -34,14 +36,16 @@ export class UsersController {
     return this.usersService.findAll(+currentPage, +limit, qs);
   }
 
+  // Endpoint để lấy thông tin người dùng theo ID
   @Public()
   @Get(':id')
   @ResponseMessage("Fetch user by id")
   async findOne(@Param('id') id: string) {
     const foundUser = await this.usersService.findOne(id);
-    return foundUser
+    return foundUser;
   }
 
+  // Endpoint để cập nhật thông tin người dùng
   @ResponseMessage("Update a User")
   @Patch()
   async update(@Body() updateUserDto: UpdateUserDto, @User() user: IUser) {
@@ -49,12 +53,14 @@ export class UsersController {
     return updatedUser;
   }
 
+  // Endpoint để xoá người dùng
   @ResponseMessage("Delete a User")
   @Delete(':id')
   remove(@Param('id') id: string, @User() user: IUser) {
     return this.usersService.remove(id, user);
   }
 
+  // Endpoint để thay đổi mật khẩu của người dùng
   @Patch('change-password')
   @ResponseMessage('Change user password')
   async changePassword(
