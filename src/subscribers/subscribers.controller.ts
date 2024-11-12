d46deccdc2a6fd28ec246281c4292d7590965516
vansang <sangnvde160492@fpt.edu.vn>
2024-11-13 01:03:06 +0700

    update subscribers

--- src/subscribers/subscribers.controller.ts
+++ src/subscribers/subscribers.controller.ts
@@ -7,57 +7,63 @@ import { IUser } from 'src/users/users.interface';
 import { ApiTags } from '@nestjs/swagger';
 
 
-@ApiTags('subscribers')
-@Controller('subscribers')
+@ApiTags('subscribers') 
+@Controller('subscribers') 
 export class SubscribersController {
-  constructor(private readonly subscribersService: SubscribersService) { }
+  constructor(private readonly subscribersService: SubscribersService) {} 
 
+  // Endpoint to create a new subscriber
   @Post()
-  @ResponseMessage("create a subscriber")
+  @ResponseMessage("create a subscriber") 
   create(@Body() createSubscriberDto: CreateSubscriberDto, @User() user: IUser) {
-    return this.subscribersService.create(createSubscriberDto, user);
+    return this.subscribersService.create(createSubscriberDto, user); 
   }
 
+  // Endpoint to retrieve a subscriber's subject without checking permissions
   @Post("subject")
   @ResponseMessage("Get subscriber's subject")
-  @SkipCheckPermission()
+  @SkipCheckPermission() 
   getUserSubject(@Query("id") id: string) {
-    return this.subscribersService.getSubject(id);
+    return this.subscribersService.getSubject(id); 
   }
 
+  // Endpoint to fetch a paginated list of subscribers
   @Get()
   @ResponseMessage("fetch list subscriber with paginate")
   findAll(
-    @Query("current") currentPage: string, //const currentPage: string = req.query.page;
-    @Query("pageSize") limit: string,
-    @Query() qs: string,
+    @Query("current") currentPage: string, 
+    @Query("pageSize") limit: string, 
+    @Query() qs: string 
   ) {
-    return this.subscribersService.findAll(+currentPage, +limit, qs);
+    return this.subscribersService.findAll(+currentPage, +limit, qs); 
   }
 
+  // Endpoint to fetch a single subscriber by their ID
   @Get(':id')
   @ResponseMessage("fetch a subscriber by id")
   findOne(@Param('id') id: string) {
-    return this.subscribersService.findOne(id);
+    return this.subscribersService.findOne(id); 
   }
 
+  // Endpoint to update an existing subscriber, skips permission check
   @Patch(':id')
   @ResponseMessage("Update a subscriber")
-  @SkipCheckPermission()
+  @SkipCheckPermission() 
   update(
-    @Param("id") id: string,
-    @Body() updateSubscriberDto: UpdateSubscriberDto,
-    @User() user: IUser
+    @Param("id") id: string, 
+    @Body() updateSubscriberDto: UpdateSubscriberDto, 
+    @User() user: IUser 
   ) {
-    return this.subscribersService.update(id, updateSubscriberDto, user);
+    return this.subscribersService.update(id, updateSubscriberDto, user); 
   }
 
+  // Endpoint to delete a subscriber
   @Delete(':id')
   @ResponseMessage("Delete a subscriber")
   remove(
-    @Param('id') id: string,
-    @User() user: IUser
+    @Param('id') id: string, 
+    @User() user: IUser 
   ) {
-    return this.subscribersService.remove(id, user);
+    return this.subscribersService.remove(id, user); // Calls service to remove the subscriber by ID
   }
 }
